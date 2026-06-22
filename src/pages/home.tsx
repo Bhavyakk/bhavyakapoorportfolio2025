12ffc555aad3248e17e801a864b6c55750d82ccb
Bhavyakk <99263962+Bhavyakk@users.noreply.github.com>
2026-06-22 14:12:03 +0530

    Revert "feat: add lenis smooth scrolling, cinematic noise overlay, and hide scrollbars for 10/10 polish"
    This reverts commit 9c7e44eea8de7a3eba4377a0f56961c11b5ea7e8.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,3 @@
-import { ReactLenis } from "@studio-freight/react-lenis";
-import { NoiseOverlay } from "@/components/ui/noise-overlay";
 import { ParticleBackground } from "@/components/ui/particle-background";
 import { ScrollIndicator } from "@/components/ui/scroll-indicator";
 import { Navigation } from "@/components/ui/navigation";
@@ -13,20 +11,17 @@ import { Footer } from "@/components/sections/footer";
 
 export default function Home() {
   return (
-    <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true }}>
-      <div className="relative">
-        <NoiseOverlay />
-        <ScrollIndicator />
-        <ParticleBackground />
-        <Navigation />
-        <Hero />
-        <About />
-        <Education />
-        {/* <Experience /> */}
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
-    </ReactLenis>
+    <div className="relative">
+      <ScrollIndicator />
+      <ParticleBackground />
+      <Navigation />
+      <Hero />
+      <About />
+      <Education />
+      {/* <Experience /> */}
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
